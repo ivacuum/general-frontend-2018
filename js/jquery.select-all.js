e73c9c17711fce64428f656f0fa82c8d1f3a261b
Sergey Pankov <vacuum@ivacuum.ru>
2018-02-17 05:13:36 +0300

    Возможность генерации события после клика

--- js/jquery.select-all.js
+++ js/jquery.select-all.js
@@ -1,4 +1,8 @@
 // Выбрать все
 $(document).on('click', '.js-select-all', function () {
   $(this.dataset.selector).prop('checked', this.checked)
+
+  if (this.dataset.triggerOnclick) {
+    $(document).trigger(this.dataset.triggerOnclick)
+  }
 })
